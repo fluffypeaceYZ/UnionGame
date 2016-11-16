fbc99ffad021c16394a71dcbfda8e051dfd40ea2
Unknown <buzgata@yahoo.ca>
2016-11-15 21:48:49 -0500

    Beginning

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+		 
+			GetComponent<Animation>().Play ();
+		
+		}
+
+		if (Input.GetMouseButtonDown) {
+
+			GetComponent<Animation>().Play ();
+
+		}
+	
+	}
+}
